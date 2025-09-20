c21d16349243e8753406733d6aba75788f5cdf2f
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-20 20:05:45 -0300

    feat: create home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import BackgroundRippleWithText from '$lib/components/BackgroundRippleEffect/BackgroundRippleWithText.svelte';
-	// import Menu from '$lib/components/DockMenu/Menu.svelte';
 </script>
 
 <div class="flex flex-col dark:bg-black">
