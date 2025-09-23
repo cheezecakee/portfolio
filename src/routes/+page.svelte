2cf64da03568cf548709fadf9f35ac549df6220c
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-23 12:36:25 -0300

    feat: add page title and meta description for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,14 @@
 	import BackgroundRippleWithText from '$lib/components/BackgroundRippleEffect/BackgroundRippleWithText.svelte';
 </script>
 
+<svelte:head>
+	<title>Home</title>
+	<meta
+		name="description"
+		content="Cheezecake's personal portfolio showcasing projects, and skills"
+	/>
+</svelte:head>
+
 <div class="flex flex-col dark:bg-black">
 	<BackgroundRippleWithText />
 </div>
