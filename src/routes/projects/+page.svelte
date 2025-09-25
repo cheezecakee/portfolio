e7e9fc31cf3ee00d4a55b95efb58b3aa04bb77a1
cheezecakee <anabel.pmoraes@gmail.com>
2025-09-25 13:54:54 -0300

    style: minor Projects page UI fix

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -13,7 +13,7 @@
 	<meta name="description" content="Explore my development projects and coding journey." />
 </svelte:head>
 
-<div class="mx-auto flex flex-col items-center justify-center space-y-6 px-5 md:px-30 lg:px-80">
+<div class="mx-auto flex flex-col items-center justify-center space-y-6 px-5 md:px-30 lg:px-60">
 	<div class="w-full space-y-2">
 		<span
 			class="pointer-events-none w-full bg-gradient-to-b from-gray-100 to-gray-300/80 bg-clip-text px-5 text-start text-3xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-gray-100 dark:to-gray-300/50"
