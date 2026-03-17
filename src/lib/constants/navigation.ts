9414745d01dff0b11050d8ceeb5dee69fad81be0
cheezecakee <anabel.pmoraes@gmail.com>
2026-03-17 11:50:10 -0300

    feat: Added Blog page

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -1,6 +1,7 @@
 export const NAV_HOME = '/' as const;
 export const NAV_ABOUT = '/about' as const;
 export const NAV_PROJECTS = '/projects' as const;
+export const NAV_BLOG = '/blog' as const;
 export const GITHUB_URL = 'https://github.com/cheezecakee' as const;
 export const LINKEDIN_URL = 'https://www.linkedin.com/in/anabel-pereira-moraes-00806a156/' as const;
 export const EMAIL_URL = 'anabel.pmoraes@gmail.com' as const;
