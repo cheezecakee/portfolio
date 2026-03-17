33493e5898c479336363dccc1fa85fa6f068ac17
cheezecakee <anabel.pmoraes@gmail.com>
2026-03-17 13:56:16 -0300

    refactor: background now uses Snippet

--- src/lib/components/BentoGrid/BentoGrid.svelte
+++ src/lib/components/BentoGrid/BentoGrid.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
 	import { cn } from '$lib/utils';
-	let className: string = '';
-	export { className as class };
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface BentoGridProps extends HTMLAttributes<HTMLDivElement> {
+		children: Snippet;
+		class?: string;
+	}
+
+	let { children, class: className, ...props }: BentoGridProps = $props();
 </script>
 
-<div class={cn('grid w-full auto-rows-min grid-cols-1 gap-6 md:grid-cols-2', className)}>
-	<slot></slot>
+<div class={cn('grid w-full auto-rows-min grid-cols-1 gap-6 md:grid-cols-2', className)} {...props}>
+	{@render children()}
 </div>
