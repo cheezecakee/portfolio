28118cdd32c57aa8ccc05a4821aff914a6d41f5e
cheezecakee <anabel.pmoraes@gmail.com>
2026-03-17 11:51:16 -0300

    feat: Added MDsveX config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,25 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    extensions: ['.svelte', '.svx', '.md'],
+	preprocess: [
+        vitePreprocess(),
+        mdsvex({ 
+            extensions: ['.svx', '.md'],
+            layout: {
+                blog: resolve(__dirname, './src/lib/blog/layout.svelte'),
+            },
+        })
+    ],
 	kit: { adapter: adapter() }
 };
 
